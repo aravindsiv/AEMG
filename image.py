dbb4d0fb23772c8909f612a27fefd8a3421f95d7
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2022-09-28 16:52:09 -0400

    Update image.py

--- image.py
+++ image.py
@@ -40,39 +40,13 @@ x = torch.tensor([0.1,0.1],dtype=torch.float32)
 x = model.decoder(x)
 x = x.detach().numpy()
 x = x * (tf_bounds[:,1] - tf_bounds[:,0]) + tf_bounds[:,0]
-print("x,y,xdot,ydot: ",x)
 x = env.inverse_transform(x)
-print("theta, thetadot: ",x)
-y = env.transform(x)
-print("x,y,xdot,ydot: ",y)
 
-traj = [x]
 for i in range(100):
+    # This is the g function
     x = env.step(x)
-    traj.append(x)
 
-traj = np.array(traj)
-
-plt.figure(figsize=(8,8))
-plt.xlim(-np.pi,np.pi)
-plt.ylim(-2*np.pi,2*np.pi)
-plt.xlabel("theta")
-plt.ylabel("theta_dot")
-plt.plot(traj[:,0],traj[:,1])
-plt.scatter(traj[0,0],traj[0,1],c="r")
-plt.show()
-
-plt.figure(figsize=(8,8))
-
-tf_traj = np.vstack([env.transform(s) for s in traj])
-tf_traj = (tf_traj - tf_bounds[:,0]) / (tf_bounds[:,1] - tf_bounds[:,0])
-tf_traj = torch.from_numpy(tf_traj).float()
-encoding = model.encoder(tf_traj).detach().numpy()
-plt.plot(encoding[:,0],encoding[:,1],color='black')
-plt.scatter(encoding[0,0],encoding[0,1],c="r")
-
-plt.xlim(-1,1)
-plt.ylim(-1,1)
-plt.xlabel("encoding 1")
-plt.ylabel("encoding 2")
-plt.show()
+x = (x - tf_bounds[:,0]) / (tf_bounds[:,1] - tf_bounds[:,0])
+x = torch.from_numpy(x).float()
+encoding = model.encoder(x).detach().numpy()
+print(x)
