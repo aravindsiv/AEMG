31d8b41f38c62fd244d358873b93224240f23883
aravindsiv <aravind.siva@rutgers.edu>
2022-11-16 11:40:18 -0500

    Plotting morse tiles in the original space

--- plot_morse.py
+++ plot_morse.py
@@ -33,7 +33,14 @@ dynamics = torch.load(f"{MODEL_PATH}/dynamics_{mode}_0.1_20steps_1M{'_warmup' if
 
 roa_file = "RoA_"+mode+".csv"
 
+class BoxCenter:
+    def __init__(self,corner_low,corner_high,morse_set):
+        self.corner_low = corner_low
+        self.corner_high = corner_high
+        self.morse_set = morse_set
+
 box_dims = []
+box_corners = []
 box_data = defaultdict(list)
 with open(roa_file, 'r') as f:
     line_counter = 0
@@ -43,41 +50,46 @@ with open(roa_file, 'r') as f:
         elif line_counter > 2 and not l.startswith("Tile"):
             raw = [float(e) for e in l.split(",")]
             box_data[int(raw[1])].append(raw[2:])
+            corner_low = np.array([raw[2],raw[3]])
+            corner_high = np.array([raw[4],raw[5]])
+            box_corners.append(BoxCenter(corner_low, corner_high, int(raw[1])))
         line_counter += 1
 
+print("Number of boxes: ", len(box_corners))
 num_morse_sets = len(box_data.keys())
-
-plt.figure(figsize=(8,8))
-plt.xlim(-1,1)
-plt.ylim(-1,1)
-
 cmap = matplotlib.cm.get_cmap('viridis', 256)
 cmap_norm = matplotlib.colors.Normalize(vmin=0, vmax=num_morse_sets-1)
 
+def find_box(x, y):
+    # Find the box that contains the point (x,y)
+    for box in box_corners:
+        if x >= box.corner_low[0] and x <= box.corner_high[0] and y >= box.corner_low[1] and y <= box.corner_high[1]:
+            return box.morse_set
+    return -1
 
-for k in box_data.keys():
-    if k < 0: continue
-    clr = matplotlib.colors.to_hex(cmap(cmap_norm(k)))
-    for elt in box_data[k]:
-        rect = Rectangle((elt[0], elt[1]), box_dims[0], box_dims[1], edgecolor='black', facecolor=clr, alpha=0.4)
-        plt.gca().add_patch(rect)
 
-plt.show()
+xs = np.linspace(-np.pi, np.pi, 41)
+ys = np.linspace(-2*np.pi, 2*np.pi, 41)
 
 plt.figure(figsize=(8,8))
 plt.xlim(-np.pi, np.pi)
 plt.ylim(-2*np.pi, 2*np.pi)
 
-for k in box_data.keys():
-    if k < 0: continue
-    clr = matplotlib.colors.to_hex(cmap(cmap_norm(k)))
-    for elt in box_data[k]:
-        rect_center = 0.5 * np.array([elt[0] + elt[2], elt[1] + elt[3]])
-        x = torch.tensor(rect_center, dtype=torch.float32)
-        x = decoder(x)
-        x = x.detach().numpy()
-        x = x * (tf_bounds[:,1] - tf_bounds[:,0]) + tf_bounds[:,0]
-        x = env.inverse_transform(x)
-        plt.scatter(x[0], x[1], color=clr, alpha=0.4)
+for i in tqdm(range(xs.shape[0])):
+    for j in range(ys.shape[0]):
+        state = [xs[i], ys[j]]
+        x = env.transform(state)
+        x = (x - tf_bounds[:,0]) / (tf_bounds[:,1] - tf_bounds[:,0])
+        x = torch.tensor(x, dtype=torch.float32)
+        z = encoder(x)
+        z = z.detach().numpy()
+        morse_set = find_box(z[0], z[1])
+        color = cmap(cmap_norm(morse_set))
+        if morse_set != -1:
+            plt.scatter(xs[i], ys[j], color=color,alpha=0.4,marker="s")
 
+plt.xlabel("theta")
+plt.ylabel("thetadot")
+plt.title("Morse sets")
 plt.show()
+
