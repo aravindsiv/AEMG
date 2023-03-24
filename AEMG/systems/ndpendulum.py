3ebf94839ecd8581aa5b1980181b412983ef1b1c
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-03-24 18:00:50 -0400

    Pushing the transformation of the ndpendulum

---
+++ AEMG/systems/ndpendulum.py
@@ -0,0 +1,29 @@
+import numpy as np 
+from AEMG.systems.system import BaseSystem
+
+class NdPendulum(BaseSystem):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "pendulum"
+
+        self.state_bounds = np.array([[-np.pi, np.pi], [-2*np.pi, 2*np.pi]])
+        
+        # Create an equispaced grid of points in the state space
+        self.centers = np.array([[-np.pi,-2*np.pi],[-np.pi,0],[-np.pi,2*np.pi],[0,-2*np.pi],[0,0],[0,2*np.pi],[np.pi,-2*np.pi],[np.pi,0],[np.pi,2*np.pi]])
+
+        self.l = 0.5
+    
+    def transform(self,s):
+        pt = np.zeros((self.centers.shape[0],))
+        for i in range(self.centers.shape[0]):
+            pt[i] = np.exp(-np.linalg.norm(s-self.centers[i])**2)
+        return pt
+    
+    def inverse_transform(self,s):
+        return s
+    
+    def get_true_bounds(self):
+        return NotImplementedError
+    
+    def get_bounds(self):
+        return NotImplementedError
--- AEMG/systems/utils.py
+++ AEMG/systems/utils.py
@@ -1,4 +1,5 @@
 from AEMG.systems.pendulum import Pendulum
+from AEMG.systems.ndpendulum import NdPendulum
 from AEMG.systems.physics_pendulum import PhysicsPendulum
 from AEMG.systems.hopper import Hopper
 from AEMG.systems.cartpole import Cartpole
@@ -7,6 +8,8 @@ from AEMG.systems.discrete_map import DiscreteMap
 def get_system(name, **kwargs):
     if name == "pendulum":
         system = Pendulum(**kwargs)
+    elif name == "pendulum":
+        system = NdPendulum(**kwargs)
     elif name == "physics_pendulum":
         system = PhysicsPendulum(**kwargs)
     elif name == "hopper":
