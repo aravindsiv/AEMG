bc39290f6d60aeb96fd496de75eb27e002ba0841
Ewerton-Vieira <65744870+Ewerton-Vieira@users.noreply.github.com>
2023-07-05 12:30:53 -0400

    Update utils.py

--- AEMG/systems/utils.py
+++ AEMG/systems/utils.py
@@ -1,6 +1,5 @@
 from AEMG.systems.pendulum import Pendulum
 from AEMG.systems.ndpendulum import NdPendulum
-from AEMG.systems.physics_pendulum import PhysicsPendulum
 from AEMG.systems.hopper import Hopper
 from AEMG.systems.cartpole import Cartpole
 from AEMG.systems.discrete_map import DiscreteMap
