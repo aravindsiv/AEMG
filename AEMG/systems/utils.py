d1ba5e2ba6d366c27ec56ca1b7d64c3ee896edbe
Ewerton-Vieira <65744870+Ewerton-Vieira@users.noreply.github.com>
2023-07-05 12:13:41 -0400

    Update utils.py

--- AEMG/systems/utils.py
+++ AEMG/systems/utils.py
@@ -10,8 +10,6 @@ def get_system(name, dims=10, **kwargs):
         system = Pendulum(**kwargs)
     elif name == "ndpendulum" and dims is not None:
         system = NdPendulum(dims, **kwargs)
-    elif name == "physics_pendulum":
-        system = PhysicsPendulum(**kwargs)
     elif name == "hopper":
         system = Hopper(**kwargs)
     elif name == "cartpole":
