08242bea85987c0414f5324a22baa6302706da02
ewerton-vieira <ewertonrvieira@gmail.com>
2023-08-05 01:17:00 -0400

    fixing the sampling

--- AEMG/systems/cartpole.py
+++ AEMG/systems/cartpole.py
@@ -9,7 +9,8 @@ class Cartpole(BaseSystem):
 
     def transform(self,x):
         theta = x[1]
-        x[1] = np.arctan2(np.sin(theta),np.cos(theta))
+        # x[1] = np.arctan2(np.sin(theta),np.cos(theta))
+        x = [x[0], np.cos(theta), np.sin(theta), x[2], x[3]]
         return x
     
     def achieved_goal(self,s):
@@ -17,7 +18,8 @@ class Cartpole(BaseSystem):
         return (diff < 0.1)
     
     def get_true_bounds(self):
-        return NotImplementedError
+        # bounds normalized by arctan2
+        return np.array([[-1, 1], [-np.pi, np.pi], [-1.27, 1.27], [-3.04, 3.04]])
     
     def get_bounds(self):
         return NotImplementedError
--- examples/get_MG_RoA.py
+++ examples/get_MG_RoA.py
@@ -1,9 +1,9 @@
 from AEMG.systems.utils import get_system
 from AEMG.models import *
 from AEMG.dynamics_utils import DynamicsUtils
-from AEMG.systems import pendulum
+# from AEMG.systems import pendulum
 from AEMG.data_utils import DynamicsDataset
-from torch.utils.data import DataLoader
+# from torch.utils.data import DataLoader
 import argparse
 
 import CMGDB_util
@@ -74,11 +74,14 @@ def main(args, config, experiment_name):
 
     MG_util = CMGDB_util.CMGDB_util()
 
+    system = get_system(config['system'], config['high_dims'])
+
     sb = args.sub
     number_of_steps = np.ceil(12 / config['step'])  # at least 0.6 seconds in total
     if config['system'] == "discrete_map":
         number_of_steps = 1
-    uniform_sample = True
+
+    
 
     subdiv_init = subdiv_min = subdiv_max = sb  # non adaptive proceedure
 
@@ -90,8 +93,9 @@ def main(args, config, experiment_name):
     upper_bounds_original_space = np.loadtxt(os.path.join(config['model_dir'], 'X_max.txt'), delimiter=',').tolist()
     print("Bounds for decoded space", lower_bounds_original_space, upper_bounds_original_space)
 
-    dim_original_space = config['high_dims']
-    grid_original_space = Grid.Grid(lower_bounds_original_space, upper_bounds_original_space, sb + 2)
+
+    # dim_original_space = config['high_dims']
+    
 
     dim_latent_space = config['low_dims']
     lower_bounds = [-1]*dim_latent_space
@@ -100,23 +104,30 @@ def main(args, config, experiment_name):
 
     grid = Grid.Grid(lower_bounds, upper_bounds, sb)
 
-    if uniform_sample:
-        grid_original_space_sample = grid_original_space.uniform_sample()
+    flag_true_bounds = False
+    if not system.get_true_bounds == NotImplementedError:  # update bounds using the true_bounds
+        lower_bounds_original_space = system.get_true_bounds()[:,0].tolist()
+        upper_bounds_original_space = system.get_true_bounds()[:,1].tolist()
+        flag_true_bounds = True
 
-        print("uniform data on the state space grid", grid_original_space_sample.shape)
-        print(grid_original_space_sample[1])
+    if args.validation_type == 'uniform': # uniform sample (high memory usage)
+        grid_original_space = Grid.Grid(lower_bounds_original_space, upper_bounds_original_space, sb + 2)
+        original_space_sample = grid_original_space.uniform_sample()
+        if flag_true_bounds:
+            original_space_sample = np.array([system.transform(i) for i in original_space_sample])
 
-        grid_latent_space_sample = dyn_utils.encode(grid_original_space_sample)
-        print("uniform data on the latent space grid", grid_latent_space_sample.shape)
-        print(grid_latent_space_sample[1])
+    elif args.validation_type == 'random': # random sample (ideal for high dim space)
+        original_space_sample = np.array([system.transform(system.sample_state()) for i in range(2**(sb+2))])
 
-    else:  # sample from trajectories
-
+    else: # sample from trajectories (ideal for large trajectory set)
         dataset = DynamicsDataset(config)
-        grid_latent_space_sample = dyn_utils.encode(dataset.Xt.numpy())
-        print("data on the latent space grid", grid_latent_space_sample.shape)
+        latent_space_sample = dyn_utils.encode(dataset.Xt.numpy())
 
-    valid_grid_latent_space = grid.valid_grid(grid_latent_space_sample)
+    latent_space_sample = dyn_utils.encode(original_space_sample)
+    print("data on the latent space", latent_space_sample.shape)
+    print(latent_space_sample[1])
+
+    valid_grid_latent_space = grid.valid_grid(latent_space_sample)
 
     def g(X):
         return dyn_tools.iterate(dyn_utils.f, X, n=number_of_steps).tolist()
@@ -151,11 +162,12 @@ def main(args, config, experiment_name):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_dir',help='Directory of config files',type=str,default='config/')
-    parser.add_argument('--config',help='Config file inside config_dir',type=str,default='discrete_map.txt')
+    parser.add_argument('--config_dir',help='Directory of config files',type=str,default='/Users/ewerton/Dropbox/Codes/AEMG/examples/config/')
+    parser.add_argument('--config',help='Config file inside config_dir',type=str,default='pendulum_lqr_1K.txt')
     parser.add_argument('--name_out',help='Name of the out file',type=str,default='out_exp')
     parser.add_argument('--RoA',help='Compute RoA',action='store_true')
     parser.add_argument('--sub',help='Select subdivision',type=int,default=14)
+    parser.add_argument('--validation_type',help='Select the type of Validation for the lantent discretization',type=str,default='random')
 
 
     args = parser.parse_args()
